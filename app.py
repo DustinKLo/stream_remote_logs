e9287d13aca32deb7bab5df44004e4f2d3c41e65
dustinklo <dustinlo.pcc@gmail.com>
2021-09-01 23:15:04 -0700

    added timeout to fabric connection
    opened the file using with keyword
    stopped all execution in the UI in the catch error handler

--- app.py
+++ app.py
@@ -33,20 +33,24 @@ def stream():
         return 'host and location must be supplied', 400
 
     try:
-        with Connection(host=host, user=REMOTE_USER, connect_kwargs=CONNECT_KWARGS) as c, c.sftp() as sftp:
+        with Connection(host=host, user=REMOTE_USER,
+                        connect_kwargs=CONNECT_KWARGS, connect_timeout=2) as c, \
+                c.sftp() as sftp:
             try:
-                f = sftp.open(location, 'r')
-                f.seek(start)
-                data = f.read()
-                if type(data) == bytes:
-                    data = data.decode('utf-8')
-                return Response(data, content_type='text/plain')
+                with sftp.open(location, 'r') as f:
+                    f.seek(start)
+                    data = f.read()
+                    if type(data) == bytes:
+                        data = data.decode('utf-8')
+                    return Response(data, content_type='text/plain')
             except FileNotFoundError as e:
-                return "file not found %s" % location, 404
+                return str(e), 404
             except Exception as e:
                 return str(e), 500
     except socket.gaierror as e:
         return str(e), 400
+    except Exception as e:
+        return str(e), 500
 
 
 if __name__ == '__main__':
